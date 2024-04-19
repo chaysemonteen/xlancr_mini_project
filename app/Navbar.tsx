e0789f2b2bd2bc8423d163ff5397913eb692afd0
Chayse Monteen <chayseraoul@gmail.com>
2024-04-20 04:15:16 +0530

    first commit

---
+++ app/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex flex-row-reverse'>
+        <div className='px-3'>Sign Up</div>  
+        <div className='px-3'>Login</div>  
+        <div className='px-3'>Blog</div>  
+    </div>
+  )
+}
+
+export default Navbar
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import Card from "./Card";
+
+export default function Home() {
+  return (
+   <Card/>
+  );
+}
