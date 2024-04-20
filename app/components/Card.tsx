44dfeaf58915d4af0dedebd84f273863a5c58392
Chayse Monteen <chayseraoul@gmail.com>
2024-04-20 22:23:44 +0530

    updates

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Button } from "@/components/ui/button"
 const Card = () => {
     
     const posts = [
@@ -38,45 +38,41 @@ const Card = () => {
             authorName: "Lourin",
             date: "Jan 4 2022",
             href: "javascript:void(0)"
-        }
+        },
+        
     ]
   return (
-    <section className="mt-12 mx-auto px-4 max-w-screen-xl md:px-8">
-    <div className="text-center">
-        <h1 className="text-3xl text-gray-800 font-semibold">
-            Blog
-        </h1>
-        <p className="mt-3 text-gray-500">
-            Blogs that are loved by the community. Updated every hour.
-        </p>
-    </div>
-    <div className="mt-12 grid gap-2 sm:grid-cols-2 lg:grid-cols-3">
-        {
-            posts.map((items, key) => (
-                <article className="max-w-md mx-auto mt-4 shadow-lg border rounded-md duration-300 hover:shadow-sm" key={key}>
-                    <a href={items.href}>
-                        <img src={items.img} loading="lazy" alt={items.title}  className="w-full h-48 rounded-t-md" />
-                        <div className="flex items-center mt-2 pt-3 ml-4 mr-2">
-                            <div className="flex-none w-10 h-10 rounded-full">
-                                <img src={items.authorLogo} className="w-full h-full rounded-full" alt={items.authorName} />
+    <section className="mt-12 mx-auto px-4 max-w-screen-xl md:px-8 relative">
+            <div className="text-center">
+                <h1 className="text-3xl text-gray-800 font-semibold">Blog</h1>
+                <p className="mt-3 text-gray-500">Blogs that are loved by the community. Updated every hour.</p>
+            </div>
+            <div className="mt-12 grid gap-2 sm:grid-cols-2 lg:grid-cols-3">
+                {posts.map((items, key) => (
+                    <article className="max-w-md mx-auto mt-4 shadow-lg border rounded-md duration-300 hover:shadow-sm" key={key}>
+                        <a href={items.href}>
+                            <img src={items.img} loading="lazy" alt={items.title} className="w-full h-48 rounded-t-md" />
+                            <div className="flex items-center mt-2 pt-3 ml-4 mr-2">
+                                <div className="flex-none w-10 h-10 rounded-full">
+                                    <img src={items.authorLogo} className="w-full h-full rounded-full" alt={items.authorName} />
+                                </div>
+                                <div className="ml-3">
+                                    <span className="block text-gray-900">{items.authorName}</span>
+                                    <span className="block text-gray-400 text-sm">{items.date}</span>
+                                </div>
                             </div>
-                            <div className="ml-3">
-                                <span className="block text-gray-900">{items.authorName}</span>
-                                <span className="block text-gray-400 text-sm">{items.date}</span>
+                            <div className="pt-3 ml-4 mr-2 mb-3">
+                                <h3 className="text-xl text-gray-900">{items.title}</h3>
+                                <p className="text-gray-400 text-sm mt-1">{items.desc}</p>
                             </div>
-                        </div>
-                        <div className="pt-3 ml-4 mr-2 mb-3">
-                            <h3 className="text-xl text-gray-900">
-                                {items.title}
-                            </h3>
-                            <p className="text-gray-400 text-sm mt-1">{items.desc}</p>
-                        </div>
-                    </a>
-                </article>
-            ))
-        }
-    </div>
-</section>  
+                        </a>
+                    </article>
+                ))}
+            </div>
+            <div className='flex justify-end fixed bottom-4 right-4'>
+                <Button variant="outline" className='rounded-full bg-blue-400'>+</Button>
+            </div>
+        </section>
   )
 }
 
